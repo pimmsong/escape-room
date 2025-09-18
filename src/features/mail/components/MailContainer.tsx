d198ddc110b7c743bd0fba9a68ea316954f90d05
subin kim <1008sb354@gmail.com>
2025-09-18 10:43:10 +0900

    feat:  메일 리스트 레이아웃

---
+++ src/features/mail/components/MailContainer.tsx
@@ -0,0 +1,49 @@
+import MailMenu from "./MailMenu";
+import MailList from "./MailList";
+import ArrowIcon from "@assets/icons/icon_arrow.svg?react";
+import { MAILLIST_VARIANTS as VARIANTS } from "../constants/variant";
+import type { Variant } from "../constants/variant";
+
+interface MailContainerProps {
+  variant?: Variant;
+}
+
+export default function MailContainer({
+  variant = "orange",
+}: MailContainerProps) {
+  return (
+    <div className={`relative w-[80%] max-w-[1024px] h-[80%] mx-auto mt-10 `}>
+      <div
+        className={`relative top-[4px] z-2 flex items-center gap-2 py-2.5 px-6 w-[350px] ${VARIANTS[variant].bg}`}
+      >
+        {VARIANTS[variant].logoIcon}
+        <h3 className="font-bold text-[23px]">LOTTECAST_EMAIL</h3>
+      </div>
+      <div
+        className={`overflow-hidden window-lg border-[4px] rounded-b-[10px] rounded-r-[10px] ${VARIANTS[variant].bg} pb-10`}
+      >
+        {/* 주소창  */}
+        <div className="flex items-center gap-4 py-2.5 px-4 border-b-[4px]">
+          <div className="flex items-center gap-2">
+            <ArrowIcon className="rotate-180" />
+            <ArrowIcon />
+          </div>
+          <div className="flex items-center gap-2 flex-1">
+            <div
+              className={`px-4 w-full py-1 border-2 rounded-[14px] text-[15px] ${VARIANTS[variant].contentBg} `}
+            >
+              www.lottecastemail.com
+            </div>
+            {VARIANTS[variant].startIcon}
+          </div>
+        </div>
+        <div className={`flex ${VARIANTS[variant].contentBg} min-h-[550px]`}>
+          <MailMenu />
+          <MailList />
+        </div>
+      </div>
+
+      {/* 주소창 */}
+    </div>
+  );
+}
