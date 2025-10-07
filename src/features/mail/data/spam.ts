fccaedee2e33b1d96ee06f9d2c31ac05682e5af4
sewon543 <sewon543@gmail.com>
2025-10-07 11:25:00 +0900

    스팸메일함 수정

--- src/features/mail/data/spam.ts
+++ src/features/mail/data/spam.ts
@@ -1,17 +1,32 @@
 export const spamMails = [
   {
     sender: "PARAN",
-    subject: "[비밀지령] 수상한 세력 즉시 저지",
-    date: "9월 30일",
+    subject: "(제목없음)",
+    date: "9월 21일",
     content: `
-    민속박물관에 민족의 정기를 위협하는 세력이 침투했다.
-    어서, 그들을 저지하도록.
-
-    혹시 수상한 동태를 보이는 자가 있거든,
-    그가 무슨 짓을 했는지 반드시 확인해라.
-
-    창문 너머 수상한 사람이 남긴 흔적이 보이는가?
+    ...캐스트 이현이 수상하다
     `,
     // image: "https://dummyimage.com/600x400/e8e8e8/999999",
+    image: "/mail/JH1.png",
+  },
+  {
+    sender: "PARAN",
+    subject: "(제목없음)",
+    date: "9월 26일",
+    content: `
+    캐스트 이현이... 수상하다
+    `,
+    // image: "https://dummyimage.com/600x400/e8e8e8/999999",
+    image: "/mail/JH2.png",
+  },
+  {
+    sender: "PARAN",
+    subject: "(제목없음)",
+    date: "9월 30일",
+    content: `
+    캐스트 이현이 수상하다...
+    `,
+    // image: "https://dummyimage.com/600x400/e8e8e8/999999",
+    image: "/mail/JH3.png",
   },
 ];
